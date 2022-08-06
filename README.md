f4fdad68576f9ccb2590d9c98a771b5c0c35f9f5
Bernardo Busatto <berbusatto@gmail.com>
2022-08-06 15:29:51 -0300

    segundo commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# banco_dadosII
+# Banco de dados II
+
+Neste repositório serão armazenados os querys e demais procedimentos executados nas aulas de BDII.
---
+++ aula01.sql
@@ -0,0 +1,23 @@
+SELECT
+    "Use MySQL database"
+AS
+    "INFO";
+
+USE mysql;
+
+SELECT
+    "Drop database aula01 if exists"
+AS 
+    "INFO";
+
+DROP DATABASE IF EXISTS aula01;
+
+SELECT "The creating of database" 
+AS "INFO";
+
+CREATE DATABASE aula01;
+
+SELECT "Use of database" 
+AS "INFO";
+
+USE "aula01";
