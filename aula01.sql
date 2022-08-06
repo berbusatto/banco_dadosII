45db7ae2a4f386c49dce294fcbee35369961e633
Bernardo Busatto <berbusatto@gmail.com>
2022-08-06 16:20:36 -0300

    script SQL para criar e inserir

--- aula01.sql
+++ aula01.sql
@@ -1,14 +1,10 @@
-SELECT
-    "Use MySQL database"
-AS
-    "INFO";
+SELECT "Use MySQL database"
+AS "INFO";
 
 USE mysql;
 
-SELECT
-    "Drop database aula01 if exists"
-AS 
-    "INFO";
+SELECT "Drop database aula01 if exists"
+AS "INFO";
 
 DROP DATABASE IF EXISTS aula01;
 
@@ -20,4 +16,47 @@ CREATE DATABASE aula01;
 SELECT "Use of database" 
 AS "INFO";
 
-USE "aula01";
+USE aula01;
+
+DROP TABLE IF EXISTS students;
+
+CREATE TABLE students (
+    id_student          INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name          VARCHAR(255) NOT NULL,
+    last_name           VARCHAR(255) NOT NULL,
+    gender              ENUM("MA", "FE"),
+    created_at          DATETIME DEFAULT CURRENT_DATE(),
+    deleted_at          DATETIME,
+    code_registration   INTEGER UNIQUE,
+    status              BOOLEAN DEFAULT true
+);
+
+DESCRIBE students;
+
+INSERT INTO students(
+    first_name,
+    last_name,
+    gender,
+    code_registration
+)
+    VALUES
+(
+    'Bernardo',
+    'Busatto',
+    'MA',
+    1
+),
+(
+    'Raphael',
+    'Stanula',
+    'MA',
+    2
+),
+(
+    'Aryon',
+    'Brunetti',
+    'MA',
+    3
+);
+
+SELECT * FROM students;
