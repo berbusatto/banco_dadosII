3bab8299acd4d6c8fa3c6be9ece6a8fda13b9ea7
Bernardo Busatto <berbusatto@gmail.com>
2022-08-21 22:20:23 -0300

    atualizando selects do trabalho

--- pets_querys.sql
+++ pets_querys.sql
@@ -1,3 +1,23 @@
 SELECT * 
 FROM pet;
 
+SELECT * 
+FROM pet
+ORDER BY createdAt DESC;
+
+SELECT * 
+FROM pet
+ORDER BY createdAt DESC
+LIMIT 3;
+
+SELECT race, count(*) as num
+FROM pet
+GROUP BY race 
+ORDER BY num DESC;
+
+SELECT name, count(*) as num
+FROM pet
+GROUP BY name
+ORDER BY num DESC
+LIMIT 10;
+
